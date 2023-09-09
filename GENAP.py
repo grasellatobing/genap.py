b21deba558e370cf181251bc52c5e3d37b9bd7f0
grasellatobing <144544732+grasellatobing@users.noreply.github.com>
2023-09-09 21:46:26 +0700

    Add files via upload
    berikut ini adalah tugas saya yang bernama Grasella Anggi Lumbantobing.

---
+++ GENAP.py
@@ -0,0 +1,36 @@
+nama ="Grasella Anggi Lumbantobing"
+nim ="230440010074"
+agama ="Kristen"
+print(f"nama saya :{nama}")
+print(f"nim saya :{nim}")
+print(f"agama saya :{agama}")
+
+#soal no.1
+print(f"soal no.1 :")
+#input panjang balok,lebar balok,dan tinggi balok
+panjangbalok=int(input("panjang balok :"))
+lebarbalok=int(input("lebar balok :"))
+tinggibalok=int(input("tinggi balok :"))
+#hitung volume balok
+volumebalok=panjangbalok*lebarbalok*tinggibalok
+#tampilkan volume balok
+print(f"hasil dari volume balok :{volumebalok}")
+
+#soal no.2
+print(f"soal no.2")
+#input jari-jari,tinggi tabung
+jarijari=float(input("jarijari :"))
+tinggitabung=float(input("tinggitabung :"))
+#hitung volume tabung
+volumetabung=22/7*jarijari*jarijari*tinggitabung
+#tampilkan volume tabung
+print(f"hasil dari volume tabung :{volumetabung}")
+
+#soal no.3
+print(f"soal no.3")
+#input jari-jari bola
+jarijaribola=float(input("jarijaribola :"))
+#hitung volume bola
+volumebola=4/3*22/7*jarijaribola*jarijaribola*jarijaribola
+#tampilkan volume bola
+print(f"hasil dari volume bola :{volumebola}")
